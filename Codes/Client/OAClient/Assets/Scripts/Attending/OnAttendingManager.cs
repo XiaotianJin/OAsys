24ee75eb2d75c534f97709567dee53b2536b3112
Jin XiaoTian <454090577@qq.com>
2017-06-08 09:39:28 +0800

    个人信息界面的考勤信息

---
+++ Codes/Client/OAClient/Assets/Scripts/Attending/OnAttendingManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Attending;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnAttendingManager : MonoBehaviour {
+
+    #region Public Pramas
+
+    public Dropdown Selection;
+
+    public Text ShowText;
+
+    #endregion
+
+    #region Btn ctrl
+
+    public void OnSearchBtn()
+    {
+        Debug.Log(string.Format("{0} : {1}", Selection.value, Selection.captionText.text));
+
+        ShowAttending(Selection.value);
+    }
+
+    #endregion
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="level">0-</param>
+    public void ShowAttending(int level)
+    {
+        List<AttendingInfo> res = AttendingInfo.GetAttendingInfo(GeneralSetting.UserName);
+        Debug.Log("!!" + res.Count);
+
+        ShowText.text = String.Empty;
+        foreach (var single in res)
+        {
+            Debug.Log(single.toString());
+            ShowText.text += single.toString() + "\n";
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
---
+++ Codes/Client/OAClient/Assets/Scripts/General/GeneralSetting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralSetting : MonoBehaviour
+{
+
+    public static string UserName = "菜鸡";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
