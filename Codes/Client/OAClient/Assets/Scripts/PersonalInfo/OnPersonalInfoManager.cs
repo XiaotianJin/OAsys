15900ebebe7a781d68ec9513664e006e0c06e252
Jin XiaoTian <454090577@qq.com>
2017-06-08 09:57:18 +0800

    个人信息功能（欠头像）

---
+++ Codes/Client/OAClient/Assets/Scripts/PersonalInfo/OnPersonalInfoManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.PersonalInfo;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnPersonalInfoManager : MonoBehaviour {
+
+    #region Public Pramas
+
+    public Text ShowText;
+
+    #endregion
+
+    // Use this for initialization
+    void Start ()
+    {
+        ShowText.text = PersonInfo.GetPersonalInfo("菜鸡").toString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
